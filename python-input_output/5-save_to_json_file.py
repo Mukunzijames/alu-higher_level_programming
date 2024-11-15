d2f18d53a559a84b0f37a6cf127cc6dacd42fb17
Mukunzijames <mukunzindahiro@gmail.com>
2024-11-15 11:38:57 +0200

    remove blank space

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""import of modules"""
+"comment"
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """function hta saves to json files"""
-    with open(filename, "w", encoding="utf-8") as a:
-        json.dump(my_obj, a)
-        
+    """comment"""
+    with open(filename, "w", encoding="UTF8") as data:
+        data.write(json.dumps(my_obj))
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""imports json module"""
+"comment"
 import json
 
 
 def load_from_json_file(filename):
-    """function that loads from json file"""
-    with open(filename, "r", encoding="utf-8") as a:
-        data = json.load(a)
-        return data
-       
-    
+    "comment"
+    with open(filename, "r", encoding="UTF8") as data:
+        return json.load(data)
