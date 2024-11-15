59f2e721bd9fadb897dae8a59793fa1ca9dd858f
Mukunzijames <mukunzindahiro@gmail.com>
2024-11-15 11:26:29 +0200

    stats

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""write script that adds to python list"""
+"comment"
+import sys
 
-from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-filename = "add_item.json"
+
 
 try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
+    list_data = load_from_json_file("add_item.json")
+except Exception:
+    list_data = []
 
-for i in argv[1:]:
-    json_list.append(argv)
-
-save_to_json_file(json_list, filename)
+for arg in sys.argv[1:]:
+    list_data.append(arg)
+save_to_json_file(list_data, "add_item.json")
