943255fbfcedd2850b2049d328a6f32690ebbe01
Mukunzijames <mukunzindahiro@gmail.com>
2025-02-07 07:46:41 +0200

    changes

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-// Read file from command line argument
-fs.readFile(process.argv[2], 'utf8', (err, contents) => {
-  // If error occurs, print error object, otherwise print file contents
-  err ? console.log(err) : console.log(contents);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
